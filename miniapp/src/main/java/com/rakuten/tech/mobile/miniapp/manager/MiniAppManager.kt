8781eca3d4b746405f545515ea4cbb75aac22efd
gopi.karmakar <gopi.karmakar@rakuten.com>
2022-09-07 21:11:38 +0900

    Removing init block

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/manager/MiniAppManager.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/manager/MiniAppManager.kt
@@ -10,10 +10,6 @@ import com.rakuten.tech.mobile.miniapp.sdkExceptionForInvalidArguments
 
 internal class MiniAppManager private constructor(): MiniAppManagerImpl() {
 
-    init {
-        // Initialize MiniAppViewHandler Object here.
-    }
-
     companion object {
 
         @SuppressLint("StaticFieldLeak")
